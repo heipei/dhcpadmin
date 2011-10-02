0b33b80f07ba5abca69b91bbed6abbab2f4566a7
Johannes Gilger <heipei@hackvalue.de>
2011-10-02 12:58:47 +0200

    assets

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,7 @@ module Dhcpadmin
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['style.css', 'hbv_admin.css', 'sorttable.js', 'application.js']
   end
 end
